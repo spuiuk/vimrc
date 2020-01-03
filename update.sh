1218da4e1641b857975826242ebd406ae022efdf
Sachin Prabhu <sprabhu@redhat.com>
2020-01-03 14:04:21 +0000

    Add descriptions for plugins

--- update.sh
+++ update.sh
@@ -9,40 +9,40 @@
 repos=(
    tpope/vim-pathogen
 
-   airblade/vim-gitgutter
+   airblade/vim-gitgutter #Plugin to show changed lines in file being modified
 #  alampros/vim-styled-jsx
 #   altercation/vim-colors-solarized
 #  ap/vim-css-color
 #  docunext/closetag.vim
 #  ervandew/supertab
 #  haya14busa/incsearch.vim
-   itchyny/lightline.vim
-   itchyny/vim-gitbranch
+   itchyny/lightline.vim #Bottom status line in vim
+   itchyny/vim-gitbranch #Used to show git branch in lightline
 #  junegunn/fzf.vim
 #  junegunn/goyo.vim
 #  mileszs/ack.vim
-   nathanaelkane/vim-indent-guides
+   nathanaelkane/vim-indent-guides #show guide line where indent tabs occur
 #  qpkorr/vim-bufkill
 #  scrooloose/nerdtree
-   sheerun/vim-polyglot
+   sheerun/vim-polyglot #Indentation and syntax support for various langs
 #  statico/vim-inform7
 #  tomasr/molokai
-   tpope/vim-commentary
+   tpope/vim-commentary #Commenting blocks of code or lines - Select in visual mode and 'gc'
 #  tpope/vim-endwise
 #  tpope/vim-eunuch
-   tpope/vim-fugitive
-   tpope/vim-repeat
+   tpope/vim-fugitive #Git handling in vim
+   tpope/vim-repeat #Repeat last command using '.'
 #  tpope/vim-rhubarb
-   tpope/vim-sleuth
+   tpope/vim-sleuth #Change shiftwidth and expandtab based on opened file
 #   tpope/vim-surround
 #  tpope/vim-sensible
 #  tpope/vim-unimpaired
-   w0rp/ale
+   w0rp/ale #linter
 #  wellle/targets.vim
-   vim-scripts/desert256.vim
-   majutsushi/tagbar
-   ludovicchabant/vim-gutentags
-   ntpeters/vim-better-whitespace
+   vim-scripts/desert256.vim #colour scheme
+   majutsushi/tagbar #Show tags in sidebar
+   ludovicchabant/vim-gutentags #Generate tags for a file
+   ntpeters/vim-better-whitespace #Show trailing whitespace
 )
 
 set -e
